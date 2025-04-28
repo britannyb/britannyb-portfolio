21092961db3519ad7d079919abdb476ee722ac32
britannyb <britbaldovino@gmail.com>
2025-04-28 09:27:53 +0800

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <>
       <div id="footer" className="flex justify-end items-center text-right">
         <div>
-          <p>created by Britanny Baldovino</p>
+          <p>created by Britanny B</p>
         </div>
         <img src="images/logo.PNG" alt="logo" width="100px" />
       </div>
